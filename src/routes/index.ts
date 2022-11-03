643f341b5d8fcefc4b03d9ae9215a48a0f0a3480
wesleyara <wes1gom@gmail.com>
2022-11-02 22:03:15 -0300

    feat: adicionando middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { prisma } from "../utils/prisma";
 import express from "express";
 import path from "path";
+import { upload } from "../middleware";
 
 export const routes = express.Router();
 
@@ -12,25 +13,48 @@ routes.get("/", (_, res) => {
   });
 });
 
-routes.get("/status", (_, res) => {
+routes.get("/static", (_, res) => {
   res.sendFile(basepath);
 });
 
-routes.post("/api/v1/status", async (req, res) => {
-  const { name, description, p, m, g, gg, key } = req.body;
+routes.post(
+  "/api/v1/items",
+  upload.array("image"),
+  async (req: any, res: any) => {
+    const { name, description, p, m, g, gg, key } = req.body;
 
-  if (key.trim() !== process.env.SECRET_KEY) {
-    return res.status(401).json({
-      error: "Access denied",
+    if (key.trim() !== process.env.SECRET_KEY) {
+      return res.status(401).json({
+        error: "Access denied",
+      });
+    }
+
+    if (!name || !description || !p || !m || !g || !gg) {
+      return res.status(400).json({
+        error: "Missing fields",
+      });
+    }
+
+    const item = await prisma.item.create({
+      data: {
+        name,
+        image: req.files[0].location,
+        description,
+        priceSmall: p,
+        priceMedium: m,
+        priceLarge: g,
+        priceXLarge: gg,
+      },
+    });
+
+    res.json({
+      message: "Updated database.",
+      data: item,
     });
   }
+);
 
-  res.json({
-    message: "Updated database.",
-  });
-});
-
-routes.get("/api/v1/status", async (_, res) => {
+routes.get("/api/v1/items", async (_, res) => {
   const data = await prisma.item.findMany();
 
   res.json(data);
